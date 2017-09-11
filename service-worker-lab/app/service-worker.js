1cb74dce64145007d0e5592a5ac65cc281e36b58
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2017-09-11 12:01:34 -0300

    knowing sw

---
+++ service-worker-lab/app/service-worker.js
@@ -0,0 +1,28 @@
+(function () {
+  'use strict';
+
+  self.addEventListener('install', function (event) {
+    console.log('Service worker installing...');
+    //good place for caching static assets
+    // TODO 3.4: Skip waiting
+  });
+
+  self.addEventListener('activate', function (event) {
+    console.log('Service worker activating...');
+    //take control of the page here
+  });
+
+  //3.3: Update the service worker
+  // Im a new service workern          nnnnn
+
+  self.skipWaiting();
+
+
+  // TODO - 4: Add fetch listener
+
+  self.addEventListener('fetch', function (event) {
+    console.log('Fetching:', event.request.url);
+  });
+
+
+})();
